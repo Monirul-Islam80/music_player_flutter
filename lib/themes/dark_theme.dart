a2e9cb2375ba3dd00acfab3403efa0e0c6f1e2fc
Monirul <monirulislam49007@gmail.com>
2025-04-13 01:03:11 +0600

    first commit

---
+++ lib/themes/dark_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkTheme = ThemeData(
+    colorScheme: const ColorScheme.dark(
+  surface: Color(0xFF121212),
+  primary: Color(0xFFBB86FC),
+  secondary: Color(0xFF03DAC6),
+  inversePrimary: Color(0xFF3700B3),
+));
---
+++ lib/themes/light_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightTheme = ThemeData(
+    colorScheme: const ColorScheme.light(
+  surface: Color(0xFFFAFAFA),
+  primary: Color(0xFF6200EE),
+  secondary: Color(0xFF03DAC6),
+  inversePrimary: Color(0xFF3700B3),
+));
